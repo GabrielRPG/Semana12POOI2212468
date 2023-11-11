da99bd358d16942e4ab7740d79f2a27220b88c08
GabrielRPG <149451382+GabrielRPG@users.noreply.github.com>
2023-11-11 14:09:27 -0500

    Add files via upload

---
+++ Ejercicio6/src/main/java/com/mycompany/ejercicio6/BusLAN.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejercicio6;
+
+/**
+ *
+ * @author Usuario
+ */
+public class BusLAN extends LAN {
+    public BusLAN() {
+        super();
+        this.topology = "Bus";
+    }
+}
