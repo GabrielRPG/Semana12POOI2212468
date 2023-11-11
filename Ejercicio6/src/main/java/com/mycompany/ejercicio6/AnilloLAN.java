9ef2dc52cdc52795328bc604d70ad0edfd842270
GabrielRPG <149451382+GabrielRPG@users.noreply.github.com>
2023-11-11 14:06:40 -0500

    Add files via upload

---
+++ Ejercicio6/src/main/java/com/mycompany/ejercicio6/AnilloLAN.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejercicio6;
+
+/**
+ *
+ * @author Usuario
+ */
+public class AnilloLAN extends LAN {
+    public AnilloLAN() {
+        super();
+        this.topology = "Ring";
+    }
+}
---
+++ Ejercicio6/src/main/java/com/mycompany/ejercicio6/HubLAN.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejercicio6;
+
+/**
+ *
+ * @author Usuario
+ */
+public class HubLAN extends LAN {
+    public HubLAN() {
+        super();
+        this.topology = "Hub";
+    }
+}
